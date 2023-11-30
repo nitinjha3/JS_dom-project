d463c94ba809f9d95cf29a1acce36da010b6604f
nitinjha3 <nitinkumarvk100@gmail.com>
2023-11-30 19:34:11 +0530

    todo complete

--- mystuffs/todo.js
+++ mystuffs/todo.js
@@ -4,6 +4,8 @@ let list=document.getElementById("ultag")
 
 function addTask()
 {
+    // let store=JSON.parse(localStorage.getItem('todo-list'))||[]
+
     if(input.value==='')
     {
         alert('Please enter your task!')
@@ -15,7 +17,11 @@ function addTask()
         span.innerHTML="\u00d7"
         list.appendChild(li)
         li.appendChild(span)
+        // store.push(input.value)
+        // localStorage.setItem('todo-list',JSON.stringify(store))
+       
     }
+    saveData()
     input.value=""
 }
 
@@ -23,9 +29,22 @@ list.addEventListener("click",function(e){
     console.log(e);
     if(e.target.nodeName==='LI'){
         e.target.classList.toggle("checked")
+        saveData()
     }
     else if(e.target.nodeName==="SPAN")
     {
         e.target.parentElement.remove();
+        saveData()
     }
 },false)
+
+function saveData(){
+    localStorage.setItem('data',list.innerHTML)
+}
+
+function display()
+{
+    list.innerHTML=localStorage.getItem('data')
+}
+
+display()
